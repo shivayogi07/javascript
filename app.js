45625494c75cbd83e15f7a071a02974362efc791
shivayogids <shivayogids5@gmail.com>
2026-03-16 21:16:32 +0530

    color change with js

---
+++ app.js
@@ -0,0 +1,19 @@
+let h1 = document.querySelector("h1");
+
+function changeColor(color, delay, nextColorChange) {
+    setTimeout(function () {
+        h1.style.color = color;
+
+        if (nextColorChange) {
+            nextColorChange();
+        }
+    }, delay);
+}
+
+changeColor("red", 1000, function () {
+    changeColor("orange", 1000, function () {
+        changeColor("green", 1000, function () {
+            changeColor("blue", 1000);
+        });
+    });
+});
