4fbdd00ecb63332e53d91099732aece1640cd5c8
shivayogids <shivayogids5@gmail.com>
2026-03-16 22:30:26 +0530

    set for promise

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
-let h1 = document.querySelector("h1");
+// let h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColorChange) {
-    setTimeout(function () {
-        h1.style.color = color;
+// function changeColor(color, delay, nextColorChange) {
+//     setTimeout(function () {
+//         h1.style.color = color;
 
-        if (nextColorChange) {
-            nextColorChange();
-        }
-    }, delay);
-}
+//         if (nextColorChange) {
+//             nextColorChange();
+//         }
+//     }, delay);
+// }
 
-changeColor("red", 1000, function () {
-    changeColor("orange", 1000, function () {
-        changeColor("green", 1000, function () {
-            changeColor("blue", 1000);
-        });
-    });
-});
+// changeColor("red", 1000, function () {
+//     changeColor("orange", 1000, function () {
+//         changeColor("green", 1000, function () {
+//             changeColor("blue", 1000);
+//         });
+//     });
+// });
 
 // callback nesting -> callback hell
+
+
+function savetoDB(data,succes,failure){
+    let inernetspeed = Math.floor(Math.random() * 10) + 1;
+    if(inernetspeed > 4){
+        succes();
+    }else{
+        failure();
+    }
+}
+
+savetoDB(
+    "apna college",() =>{
+        console.log("sucess: your data was saved");
+        savetoDB(
+            "hello world",() =>{
+                console.log("sucess2: your data2 was saved");
+                savetoDB(
+                    "shivu",() =>{
+                        console.log("sucess3: your data3 was saved");
+                    },() =>{
+                    console.log("failed3: weak connection!");
+                    }
+                );
+            },
+            () =>{
+                console.log("failed2: weak connection!");
+            }
+        );
+    },
+    () =>{
+        console.log("failed1: weak connection!");
+    }
+);
